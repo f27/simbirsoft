4e5bb3f9cf522cc19323bfa4ffc741777952d4e4
John Smoker <testgits@yandex.ru>
2021-04-18 07:12:20 +0500

    Added jsoup test

--- src/test/java/tests/tests/JsoupTests.java
+++ src/test/java/tests/tests/JsoupTests.java
@@ -1,8 +1,10 @@
 package tests.tests;
 
 import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,18 +13,28 @@ import tests.TestData;
 import java.io.IOException;
 
 import static endpoints.Endpoints.MAIN;
+import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Owner("f27")
 @Tag("jsoup")
+@DisplayName("Tests with jsoup")
 public class JsoupTests {
 
     @ParameterizedTest(name = "Проверка ссылки на FB с помощью jsoup. Язык {0}")
     @ValueSource(strings = {"ru", "en"})
     void exampleTest(String locale) throws IOException {
-        Document page = Jsoup.connect(TestData.getWebUrl() + MAIN.getPath(locale)).get();
-        assertThat(page.select("[title=Facebook]").attr("href"))
-                .isEqualTo(TestData.getMainFooterFb().get(locale));
+        String url = TestData.getWebUrl() + MAIN.getPath(locale);
+        Document page = openPage(url);
+        step("Сравниваем ссылку", () ->
+                assertThat(page.select("[title=Facebook]").attr("href"))
+                        .isEqualTo(TestData.getMainFooterFb().get(locale))
+        );
+    }
+
+    @Step("Открываем страницу")
+    private Document openPage(String url) throws IOException {
+        return Jsoup.connect(url).get();
     }
 
 }
