53cdb02a2024d862eb093bdc7b9b0285e46aa9fa
John Smoker <testgits@yandex.ru>
2021-04-18 17:57:02 +0500

    Added allure notofications

--- src/test/java/tests/tests/SelenideTests.java
+++ src/test/java/tests/tests/SelenideTests.java
@@ -25,7 +25,7 @@ public class SelenideTests extends TestBase {
     }
 
     @Feature("Главная страница")
-    @ParameterizedTest
+    @ParameterizedTest(name = "Проверка главной страницы. Язык {0}")
     @MethodSource("localesProvider")
     void mainPageTest(String locale) {
         Map<String, String> expectedData = new HashMap<String, String>() {{
